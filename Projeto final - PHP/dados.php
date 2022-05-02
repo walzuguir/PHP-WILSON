f999211c0dba8bc3c30ec49ad4da7ac966dc39cf
Wilson Alzuguir <Walzuguir@gmail.com>
2022-05-02 15:28:28 -0300

    Integração do banco de dados (parte 2)
    Integração do banco de dados ao projeto do site em PHP.

---
+++ Projeto final - PHP/dados.php
@@ -0,0 +1,51 @@
+<?php
+    
+    $services = array(
+        (object) [
+            'classe' => 'bi-collection',
+            'titulo' => 'Parte 1',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+        ],
+        (object) [
+            'classe' => 'bi-building',
+            'titulo' => 'Parte 2',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+        ],
+        (object) [
+            'classe' => 'bi-toggles2',
+            'titulo' => 'Parte 3',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+        ],
+        (object) [
+            'classe' => 'bi-toggles2',
+            'titulo' => 'Parte 4',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+        ]
+    );
+    $blogs = array(
+
+        (object) [
+            'classe' => 'News',
+            'titulo' => 'Blog post title',
+            'descricao' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+            'nome' => 'Rown Kill',
+            'data' => 'June 19, 2022 &middot; 3 min read'
+        ],
+        (object) [
+            'classe' => 'News 2',
+            'titulo' => 'Another blog post title',
+            'descricao' => 'This text is a bit longer to illustrate the adaptive height of each card. Some quick example text to build on the card title and make up the bulk of the cards content.',
+            'nome' => 'Lautaro Arlinez',
+            'data' => 'July 14, 2022 &middot; 4 min read'
+        ],
+        (object) [
+            'classe' => 'News 3',
+            'titulo' => 'The last blog post title is a little bit longer than the others',
+            'descricao' => 'Some more quick example text to build on the card title and make up the bulk of the cards content.',
+            'nome' => 'Joshua Martinez',
+            'data' => 'April 25, 2022 &middot; 8 min read'
+        ]
+    );
+
+?>
+
